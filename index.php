f084d098aab6929cace7c7ae6409c74fefd04274
Fatih Yenice <fatih.ydev@gmail.com>
2025-03-07 11:20:30 +0100

    modification

--- index.php
+++ index.php
@@ -12,14 +12,26 @@ $recordset = $stmt->fetchAll(PDO::FETCH_ASSOC)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BdShop - Accueil</title>
     <link rel="icon" href="/image/favicon.png">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
     <script src="/js/script.js" defer></script>
+    <script src="/js/bootstrap.js" defer></script>
 </head>
 <body>
+
     <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
  
-     
+     <main class="container">
+        <div class="row">
+            <?php foreach($recordset as $row){ ?> 
+                 <article class="col-12 col-md-6 col-lg-4 col-xl-3">
+                    <span><?= $row['product_name']; ?></span>
+                </article>
+            <?php } ?>
+        </div>
+    </main>
     
     <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'; ?>
+
 </body>
 </html>
